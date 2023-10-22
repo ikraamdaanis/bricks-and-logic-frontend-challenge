4b929e70ac59797aaa69993c3afa384080ee5b6b
Ikraam <ibndaanis@gmail.com>
2023-10-22 16:19:25 +0100

    infinite scroll

---
+++ components/InfiniteScroller.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+interface InfiniteScrollProps extends React.HTMLAttributes<HTMLDivElement> {
+  fetchNextPage: () => void;
+  hasNextPage: boolean;
+  loadingMessage: React.ReactNode;
+  endingMessage?: React.ReactNode;
+}
+
+/**
+ * InfiniteScroller is a React component that enables infinite scrolling. It observes
+ * an intersection target and triggers a fetch when that target becomes visible.
+ */
+export const InfiniteScroller = React.forwardRef<
+  HTMLDivElement,
+  InfiniteScrollProps
+>(function InfiniteScroller(
+  {
+    fetchNextPage,
+    hasNextPage,
+    endingMessage = null,
+    loadingMessage,
+    children,
+    ...props
+  },
+  ref
+) {
+  const observerTarget = React.useRef(null);
+
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        if (entries[0]?.isIntersecting) fetchNextPage();
+      },
+      { threshold: 1 }
+    );
+
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+
+    return () => observer.disconnect();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div ref={ref} {...props} style={{ overflowAnchor: "none" }}>
+      {children}
+      <div ref={observerTarget} />
+      {hasNextPage ? loadingMessage : endingMessage}
+    </div>
+  );
+});
--- features/characters/components/CharactersContainer.tsx
+++ features/characters/components/CharactersContainer.tsx
@@ -1,46 +1,40 @@
 "use client";
 
-import { Button } from "components/ui/button";
+import { InfiniteScroller } from "components/InfiniteScroller";
 import { CharacterCard } from "features/characters/components/CharacterCard";
 import { useFetchCharacters } from "features/characters/hooks/useFetchCharacters";
 import { Character } from "features/characters/types";
+import { Loader2 } from "lucide-react";
 
+/**
+ * A container component for displaying a list of characters fetched
+ * from the Rick and Morty API. Uses an infinite query so users can
+ * keep scrolling and load more characters.
+ */
 export const CharactersContainer = () => {
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    fetchPreviousPage,
-    hasPreviousPage
-  } = useFetchCharacters();
+  const { data, fetchNextPage, hasNextPage } = useFetchCharacters();
 
-  console.log(data, hasPreviousPage);
+  console.log(data);
 
   const rows = (data?.pages.flatMap(page => page?.results || []) ||
     []) as Character[];
 
   return (
-    <div className="mx-auto w-full max-w-screen-xl">
-      Characters
-      <div className="grid grid-cols-3 gap-4">
-        {rows.map(character => {
-          return <CharacterCard key={character.id} character={character} />;
-        })}
-      </div>
-      <Button
-        className="text-lg"
-        onClick={() => fetchPreviousPage()}
-        disabled={!hasPreviousPage}
+    <div className="mx-auto flex w-full max-w-screen-xl flex-col items-center py-4">
+      <InfiniteScroller
+        fetchNextPage={fetchNextPage}
+        hasNextPage={hasNextPage || false}
+        loadingMessage={
+          <Loader2 className="my-4 h-6 w-6 animate-spin text-zinc-500" />
+        }
+        className="scroller flex w-full flex-1 flex-col items-center overflow-auto"
       >
-        Prev
-      </Button>
-      <Button
-        className="text-lg"
-        onClick={() => fetchNextPage()}
-        disabled={!hasNextPage}
-      >
-        Next
-      </Button>
+        <div className="mx-auto grid w-full grid-cols-3 gap-4">
+          {rows.map(character => {
+            return <CharacterCard key={character.id} character={character} />;
+          })}
+        </div>
+      </InfiniteScroller>
     </div>
   );
 };
