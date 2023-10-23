bd7a2c65f2a64c564318465ab566724be3e72bf5
Ikraam <ibndaanis@gmail.com>
2023-10-23 22:46:19 +0100

    header z-index

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 /** Header that displays the logo/title and links to the homepage. */
 export const Header = () => {
   return (
-    <header className="border-b-brand-500 sticky top-0 h-20 w-full border-b bg-white/70 backdrop-blur">
+    <header className="border-b-brand-500 sticky top-0 z-10 h-20 w-full border-b bg-white/70 backdrop-blur">
       <nav className="mx-auto flex h-20 w-full max-w-screen-lg items-center px-4 xl:px-0">
         <Link href="/" className="flex items-center gap-2">
           <RickAndMortyIcon className="fill-brand-600 h-6 w-6 lg:h-8 lg:w-8" />
